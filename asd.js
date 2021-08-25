a8ed20447bab7fbab8ca9aea20a8f24fd9bc39ff
hasanamane <89529514+hasanamane@users.noreply.github.com>
2021-08-25 16:54:05 +0300

    Add files via upload

---
+++ asd.js
@@ -0,0 +1,10 @@
+function openNav()
+{
+    document.getElementById("myasidenav").style.width="305px";
+    document.getElementById("main").style.width="305px";
+}
+function closeNav()
+{
+    document.getElementById("myasidenav").style.width="0px";
+    document.getElementById("main").style.width="0px";
+}
